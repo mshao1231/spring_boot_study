015f74694c35cdec2470c679d1a4e811fdaa568c
sjh <1183008535@qq.com>
2022-08-10 16:22:05 +0800

    other-team 第二次提交Test

--- SpringSecurity_01/src/main/java/com/aku/controller/PageController.java
+++ SpringSecurity_01/src/main/java/com/aku/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.aku.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /*专门用于处理页面响应的控制器*/
 @Controller
--- SpringSecurity_01/src/main/java/com/aku/test/Test.java
+++ SpringSecurity_01/src/main/java/com/aku/test/Test.java
@@ -7,6 +7,9 @@ public class Test {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); //这里使用SpringSecurity提供的BCryptPasswordEncoder
         System.out.println(encoder.encode("123456"));
+        System.out.println("========");
+        System.out.println("master....");
+        System.out.println("hot-fix.....");
         System.out.println("other-team....");
     }
 
