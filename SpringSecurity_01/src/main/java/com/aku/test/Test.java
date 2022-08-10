61f903babcae42bc92b38604aea18b569458b0e5
sjh <1183008535@qq.com>
2022-08-10 16:04:17 +0800

    other-team 第一次提交Test

--- SpringSecurity_01/src/main/java/com/aku/test/Test.java
+++ SpringSecurity_01/src/main/java/com/aku/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); //这里使用SpringSecurity提供的BCryptPasswordEncoder
         System.out.println(encoder.encode("123456"));
+        System.out.println("other-team....");
     }
 
 }
